66a1be2020f280500388b582672c647672ea1150
CatL <catherinelui.2@gmail.com>
2020-09-13 13:23:31 -0600

    debugging

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ const addEmployee = () => {
         "SELECT employee.first_name, employee.last_name, role.title, department.name AS Department, role.salary, CONCAT(manager.mgr_first, manager.mgr_last) AS Manager ";
     query += "FROM department INNER JOIN role ON role.department_id = department.id ";
     query += "INNER JOIN employee ON employee.role_id = role.id ";
-    query += "INNER JOIN manager ON manager.id = employee.manager_id ";
+    query += "LEFT JOIN manager ON manager.id = employee.manager_id ";
 
     connection.query(query, (err, results) => {
         if (err) throw err;
@@ -188,30 +188,33 @@ const addEmployee = () => {
                             chosenRoleID = results[i].id;
                         }
                     }
-                    
-                    connection.query("SELECT id, CONCAT(manager.mgr_first, manager.mgr_last) AS Manager FROM orgChart_db.manager; ", (err,result) => {
-                        let chosenMgrID;
-                    for (var j = 0; j < result.length; j++) {
-                        if (result[j].Manager === answer.whichMgr) {
-                            chosenMgrID = result[j].id;
-                        }
-                    }
+
                     connection.query(
-                        "INSERT INTO employee SET ?",
-                        {
-                            first_name: answer.empFirstName,
-                            last_name: answer.empLastName,
-                            role_id: chosenRoleID,
-                            manager_id: chosenMgrID,
-                        },
-                        (err) => {
-                            if (err) throw err;
-                            console.log("You've added an employee successfully!");
-                            start();
+                        "SELECT id, CONCAT(manager.mgr_first, manager.mgr_last) AS Manager FROM orgChart_db.manager; ",
+                        (err, result) => {
+                            let chosenMgrID;
+                            for (var j = 0; j < result.length; j++) {
+                                if (result[j].Manager === answer.whichMgr) {
+                                    chosenMgrID = result[j].id;
+                                }
+                            }
+                            connection.query(
+                                "INSERT INTO employee SET ?",
+                                {
+                                    first_name: answer.empFirstName,
+                                    last_name: answer.empLastName,
+                                    role_id: chosenRoleID,
+                                    manager_id: chosenMgrID,
+                                },
+                                (err) => {
+                                    if (err) throw err;
+                                    console.log("You've added an employee successfully!");
+                                    start();
+                                }
+                            );
                         }
                     );
-                    });
-                }); 
+                });
             });
     });
 };
@@ -219,47 +222,139 @@ const addEmployee = () => {
 // remove employee
 const removeEmployee = () => {
     //query employee first and last name and display as Employee
-    connection.query("SELECT id, CONCAT(first_name, last_name) AS Employee FROM orgChart_db.employee; ", (err, results) => {
-        if (err) throw err;
+    connection.query(
+        "SELECT id, CONCAT(first_name, last_name) AS Employee FROM orgChart_db.employee; ",
+        (err, results) => {
+            if (err) throw err;
 
-        //start prompt
-        inquirer.prompt([
-            {
-                name: "removeEmp",
-                type: "rawlist",
-                message: "Select the employe that you would like to remove.",
-                choices: () => {
-                    let choiceArrary = [];
-                    for (var i = 0; i < results.length; i++){
-                        choiceArrary.push(results[i].Employee);
-                    }
-                    return choiceArrary;
-                }
-            }
-        ])
-        .then((answer) => {
-            // get chosen employee
-            let chosenEmp;
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].Employee === answer.removeEmp) {
-                    chosenEmp = results[i].id;
-                    console.log(chosenEmp);
-
-                    connection.query("DELETE FROM employee WHERE ?", 
+            //start prompt
+            inquirer
+                .prompt([
                     {
-                        id: chosenEmp
-                    }, (err, res) => {
-                        if (err) throw err;
-                        console.log("You have sucessfully removed " + answer.removeEmp);
-                        start();
-                    })
-                }
-            }           
-        })
-    });
-}
+                        name: "removeEmp",
+                        type: "rawlist",
+                        message: "Select the employe that you would like to remove.",
+                        choices: () => {
+                            let choiceArrary = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArrary.push(results[i].Employee);
+                            }
+                            return choiceArrary;
+                        },
+                    },
+                ])
+                .then((answer) => {
+                    // get chosen employee
+                    let chosenEmp;
+                    for (var i = 0; i < results.length; i++) {
+                        if (results[i].Employee === answer.removeEmp) {
+                            chosenEmp = results[i].id;
+                            console.log(chosenEmp);
+
+                            connection.query(
+                                "DELETE FROM employee WHERE ?",
+                                {
+                                    id: chosenEmp,
+                                },
+                                (err, res) => {
+                                    if (err) throw err;
+                                    console.log("You have sucessfully removed " + answer.removeEmp);
+                                    start();
+                                }
+                            );
+                        }
+                    }
+                });
+        }
+    );
+};
 
 //update employee role
+const updateEmployeeRole = () => {
+    let query =
+        "SELECT CONCAT(employee.first_name, employee.last_name) AS Employee, role.title, department.name AS Department, role.salary, CONCAT(manager.mgr_first, manager.mgr_last) AS Manager ";
+    query += "FROM department INNER JOIN role ON role.department_id = department.id ";
+    query += "INNER JOIN employee ON employee.role_id = role.id ";
+    query += "LEFT JOIN manager ON manager.id = employee.manager_id ";
+
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+
+        connection.query("SELECT * FROM orgChart_db.role; ", (err, result) => {
+            if (err) throw err;
+            
+        
+        inquirer
+            .prompt([
+                {
+                    name: "employee",
+                    type: "rawlist",
+                    message: "Which employee would you like to update?",
+                    choices: () => {
+                        let choiceArrary = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArrary.push(results[i].Employee);
+                        }
+                        return choiceArrary;
+                    },
+                },
+                {
+                    name: "role",
+                    type: "rawlist",
+                    message: "Which role are they switching to?",
+                    choices: () => {
+                        let choiceArrary = [];
+
+                        for (var j = 0; j < result.length; j++) {
+                            choiceArrary.push(result[j].title);
+                        }
+                            return choiceArrary;
+                    },
+                },
+            ])
+            .then((answer) => {
+                connection.query("SELECT employee.id, CONCAT(employee.first_name, employee.last_name) AS Employee, employee.role_id, role.title FROM employee INNER JOIN role ON role.id = employee.role_id; ", (err, results) => {
+                    if (err) throw err;
+                    
+                    let chosenEmp; 
+
+                    for (var i = 0; i < results.length; i++) {
+                        if (results[i].Employee === answer.employee) {
+                            chosenEmp = results[i].id;
+                        }
+                    }
+                        let chosenRoleID;
+
+                        for (var j =0; j < result.length; i++) {
+                            console.log(result[i].title);
+                            if (result[i].title === answer.role){
+                                chosenRoleID = result[i].id;
+                                console.log("what is ChosenROleID  " + chosenRoleID);
+                            } else {
+                                console.log("This employee is already at this role.");
+                            }
+                        }
+
+                        connection.query("UPDATE orgChart_db.employee SET ? WHERE ? ;", [
+                            {
+                                role_id: chosenRoleID
+                            },
+                            {
+                                id: chosenEmp
+                            }
+                        ],
+                        (err) => {
+                            if (error) throw err;
+                            console.log("Employee role as been updated.");
+                        }
+                        );
+                    
+                    
+                });
+            });
+    });
+});
+};
 
 //update employee mgr`
 
